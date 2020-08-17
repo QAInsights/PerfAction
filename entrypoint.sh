ffb3cc11b69beeb95f0e61e9209d764df97f9ba5
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-17 16:51:21 -0400

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,25 +4,4 @@
 #
 # This script expects the standdard JMeter command parameters.
 #
-set -e
-freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
-s=$(($freeMem/10*8))
-x=$(($freeMem/10*8))
-n=$(($freeMem/10*2))
-export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
-
-echo "START Running Jmeter on `date`"
-echo "JVM_ARGS=${JVM_ARGS}"
-echo "jmeter args=$@"
-
-# Keep entrypoint simple: we must pass the standard JMeter arguments
-jmeter $@
-echo "END Running Jmeter on `date`"
-
-#     -n \
-#    -t "/tests/${TEST_DIR}/${TEST_PLAN}.jmx" \
-#    -l "/tests/${TEST_DIR}/${TEST_PLAN}.jtl"
-# exec tail -f jmeter.log
-#    -D "java.rmi.server.hostname=${IP}" \
-#    -D "client.rmi.localport=${RMI_PORT}" \
-#  -R $REMOTE_HOSTS
+echo "this is entrypoint"
