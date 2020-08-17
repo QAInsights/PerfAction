9dd15c45e9ea34bd96fd35f86cd57dec502974de
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-17 19:38:23 -0400

    Updated

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "This is in test.sh"
-docker run jmeter -n -t ./bin/examples/CSVSample.jmx -l mylog.log
+docker run jmeter -n -t ./bin/examples/CSVSample.jmx -l ./bin/mylog.log
 cat ./bin/mylog.log
