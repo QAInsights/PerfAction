ac0d0246c2db89e0bd4385742c8059dfe5e18a2d
NaveenKumar <catch.nkn@gmail.com>
2020-08-17 01:53:35 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JMETER_VERSION="5.1.1"
+
+# Example build line
+# --build-arg IMAGE_TIMEZONE="Europe/Amsterdam"
+docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} -t "justb4/jmeter:${JMETER_VERSION}" .
