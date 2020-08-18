e21e2ca0fde2e3e85c37fbbc7b795beb0806fe9e
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-17 20:23:52 -0400

    Updated

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "This is in test.sh"
-docker run jmeter -it -n -t ./bin/examples/CSVSample.jmx -l mylog.log
+docker run jmeter -n -t ./bin/examples/CSVSample.jmx -l mylog.log
 echo $(pwd)
-docker ps
+docker ps -a
