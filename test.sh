1d54a2b0641e0c62141aeb659647da0ff0569c12
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-17 20:15:56 -0400

    Updated

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "This is in test.sh"
-docker run jmeter -v /home/runner/work/PerfAction/PerfAction:/home/runner/work/PerfAction/PerfAction -n -t ./bin/examples/CSVSample.jmx -l mylog.log
+docker run jmeter -i -v /:/ -n -t ./bin/examples/CSVSample.jmx -l mylog.log
 echo $(pwd)
 docker ps
