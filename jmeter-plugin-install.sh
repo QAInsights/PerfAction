f9b1835213a684b19d9722bd2de163fb8d62769e
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-18 23:22:56 -0400

    Updated

--- jmeter-plugin-install.sh
+++ jmeter-plugin-install.sh
@@ -1,1 +1,2 @@
-curl -L http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar --output /opt/apache/apache-jmeter-${JMETER_VERSION/cmdrunner-2.2.jar
+#!/bin/sh
+echo "Inside Plugin"
