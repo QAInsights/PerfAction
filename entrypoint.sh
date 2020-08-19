0d23c42ae88b9cd19843fe106d1c163de3085cd0
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-18 22:35:21 -0400

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo "Execution is being started"
+echo "**************************"
 jmeter $@
 echo "Execution has been completed, please check the artifacts to download the results."
+echo "**************************"
