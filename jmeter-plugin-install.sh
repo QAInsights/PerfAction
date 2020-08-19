62f68416e61109d395c3a32ac07ae364d19e69ab
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-08-18 23:47:42 -0400

    Updated

--- jmeter-plugin-install.sh
+++ jmeter-plugin-install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 curl -L http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar --output /opt/apache/apache-jmeter-${JMETER_VERSION}/cmdrunner-2.2.jar
-
+echo "Downloaded CMDRunner"
 curl -L https://jmeter-plugins.org/get/ --output /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-1.4.jar
+echo "Downloaded Plugin Manager"
 
 java -cp /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-1.4.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
+echo "Java Step"
+echo $(pwd)
+ls
 
 chmod a+x ${JMETER_HOME}/bin/*.sh
 
