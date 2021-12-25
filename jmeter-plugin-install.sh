35b13ab881b11d26dd5c422b5d32102e2d3cdc29
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-12-25 13:16:25 -0500

    JMeter plugin install

--- jmeter-plugin-install.sh
+++ jmeter-plugin-install.sh
@@ -9,10 +9,10 @@ curl -L http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/${JMETER
 echo "Downloading Plugin Manager"
 curl -L https://jmeter-plugins.org/get/ --output ${JMETER_HOME}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar
 
-# java -cp /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
+java -cp /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
 
 cd /opt/apache/apache-jmeter-${JMETER_VERSION}/bin/
-java -jar ${JMETER_HOME}/lib/cmdrunner-${JMETER_CMD_RUNNER_VERSION}.jar --tool org.jmeterplugins.repository.PluginManagerCMD help
+java -jar ${JMETER_HOME}/lib/cmdrunner-${JMETER_CMD_RUNNER_VERSION}.jar --tool org.jmeterplugins.repository.PluginManagerCMD install jpgc-udp=0.4
 
 chmod a+x ${JMETER_HOME}/bin/*.sh
 
@@ -20,4 +20,4 @@ pwd
 
 ls -l ${JMETER_HOME}/bin/*.sh
 
-PluginsManagerCMD install jpgc-udp=0.4
+#PluginsManagerCMD install jpgc-udp=0.4
