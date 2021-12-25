a45609c422e25df02107fd6a8e2af10c85485ab0
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-12-25 13:02:15 -0500

    JMeter plugin install

--- jmeter-plugin-install.sh
+++ jmeter-plugin-install.sh
@@ -9,12 +9,10 @@ curl -L http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/${JMETER
 echo "Downloading Plugin Manager"
 curl -L https://jmeter-plugins.org/get/ --output ${JMETER_HOME}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar
 
-ls -l ${JMETER_HOME}/lib/
+# java -cp /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
 
-java -cp /opt/apache/apache-jmeter-${JMETER_VERSION}/lib/ext/jmeter-plugins-manager-${JMETER_PLUGIN_MANAGER_VERSION}.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
-
-#cd /opt/apache/apache-jmeter-${JMETER_VERSION}/bin/
-#java -jar ${JMETER_HOME}/lib/cmdrunner-2.2.jar --tool org.jmeterplugins.repository.PluginManagerCMD %*
+cd /opt/apache/apache-jmeter-${JMETER_VERSION}/bin/
+java -jar ${JMETER_HOME}/lib/cmdrunner-${JMETER_CMD_RUNNER_VERSION}.jar --tool org.jmeterplugins.repository.PluginManagerCMD help
 
 chmod a+x ${JMETER_HOME}/bin/*.sh
 
